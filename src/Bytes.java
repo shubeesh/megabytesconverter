90d2ebd08638c6d217abe1a2910b35ba472d5b70
shubeeth001 <shubeeth001@warren.k12.in.us>
2024-09-19 08:32:55 -0400

    bytes

---
+++ src/Bytes.java
@@ -0,0 +1,8 @@
+public class Bytes {
+    public static void main(String[] args) {
+        byte myByteValue = 125;
+        short myShortValue = 14;
+        int myIntValue = 45;
+        long LongTotal = 80000L - 5 * (myByteValue + myShortValue + myIntValue);
+    }
+}
